7d219c5dff4472cf0dfd456a91a43245c19afbe4
Diego Soares <dieg657@gmail.com>
2019-05-31 21:59:30 -0300

    Runge-Kutta implementacao

--- main.m
+++ main.m
@@ -1,31 +1,43 @@
 % Wanderson Santos
 % Diego dos Santos Soares - 2014.2.0028.0195-8
 
-digits(10);
-z = calcular(1, 3, 4, 0.139383);
 
+z = rungekutta(1.0, 3, 0.1, funcao);
+
+%{
 function ret = calcular(inicial, final, janela, erro)
     medido = trapezio(inicial, final, janela, @funcaoExponencial);
     fprintf('f(xU): %.6f\n', medido);
     x = calculaErroRelativo(erro, medido);
-    fprintf('O erro é de: %.6f', x); disp('%');
+    fprintf('O erro ï¿½ de: %.6f', x); disp('%');
     
     
     medido = simpsonTresOitavos(inicial, final, janela, @funcaoExponencial);
     fprintf('f(xU): %.6f\n', medido);
     x = calculaErroRelativo(erro, medido);
-    fprintf('O erro é de: %.6f', x); disp('%');
+    fprintf('O erro ï¿½ de: %.6f', x); disp('%');
     
     medido = simpsonUmTerco(inicial, final, janela, @funcaoExponencial);
     fprintf('f(xU): %.6f\n', medido);
     x = calculaErroRelativo(erro, medido);
-    fprintf('O erro é de: %.6f', x); disp('%');
+    fprintf('O erro ï¿½ de: %.6f', x); disp('%');
+    %
     
     ret = x;
 end
+%}
 
+
+    
+
+
+%{
 function res = calculaErroRelativo(real, medido)
     digits(6);
     x = vpa(abs(real - medido))/vpa(real);
     res = vpa(x * 100);
 end
+%}
+
+
+%%w = calcular(1, 3, 4, 0.139383);
